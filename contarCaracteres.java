175629b25e1d965ce65641311abd9f3fd7ef653a
Fernando Almela <falmelaruiz@gmail.com>
2025-05-08 20:27:27 +0200

    prueba
    una prueba

--- contarCaracteres.java
+++ contarCaracteres.java
@@ -1,32 +1,9 @@
-package Ejecucion;
-import java.util.Scanner;
-
-public class contarCaracteres {
+public class contarcaracteres {
     public static void main(String[] args) {
-        // Crear un objeto Scanner para leer la entrada por consola
-        Scanner scanner = new Scanner(System.in);
-        
-        // Pedir al usuario que ingrese una cadena de texto
-        System.out.print("Ingresa una cadena de texto: ");
-        String texto = scanner.nextLine();
-        
-        // Pedir al usuario el carácter que desea contar
-        System.out.print("Ingresa el caracter que deseas contar: ");
-        char caracter = scanner.next().charAt(0); // Solo lee el primer carácter
+        String a;
+        a = "Pedro rodriguez";
 
-        // Variable para contar las ocurrencias
-        int contador = 0;
-
-        // Recorrer la cadena y contar las veces que aparece el carácter
-        for (int i = 0; i < texto.length(); i++) {
-            if (texto.charAt(i) == caracter) {
-                contador++;
-            }
-        }
-
-        // Mostrar el resultado
-        System.out.println("El caracter '" + caracter + "' aparece " + contador + " veces.");
-        
-        scanner.close();
+    System.out.println(a.length());
     }
+    
 }
